e82b1a2421963c3c8d51669aa2f3af19de0370e9
xuzhicheng <xuzhicheng@xuzhichengdeMacBook-Pro.local>
2022-11-24 10:56:25 +0800

    feat: add dropdown

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import DefineOptions from 'unplugin-vue-define-options/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [DefineOptions(), vue(), vueJsx()],
   // 指定安装包的主入口为 package.json 中的 source 字段定义的路径
   resolve: {
     mainFields: ['source', 'module', 'jsnext:main', 'jsnext'],
